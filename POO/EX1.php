92eb663e65d2015325668af9f1d536d73ad586a8
gustavo_bolis <gustavo.bolis@ixcsoft.com.br>
2023-04-01 10:56:01 -0300

    feat(POO): Added POO EX1.

---
+++ POO/EX1.php
@@ -0,0 +1,58 @@
+<?php
+class pessoa{
+    private $nome;
+    private $idade;
+    private $sexo;
+
+    private static $quantidade = 0;
+
+    /**
+     * @param $nome
+     * @param $idade
+     * @param $sexo
+     */
+    public function __construct($nome, $idade, $sexo){
+        $this->nome = $nome;
+        $this->idade = $idade;
+        $this->sexo = $sexo;
+        self::$quantidade++;
+    }
+    public function get_nome(){
+        return $this->nome . PHP_EOL;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdade()
+    {
+        return $this->idade . PHP_EOL;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSexo()
+    {
+        return $this->sexo . PHP_EOL;
+    }
+
+    public static function getQuantidade() {
+        return self::$quantidade;
+    }
+
+
+}
+
+$nome = new pessoa("Kleber",6,"pouco");
+$kleber = new pessoa("Não é o kleber",66,"Masculino");
+echo $nome->get_nome();
+echo $nome->getIdade();
+echo $nome->getSexo();
+echo "Kleber compra bolo".PHP_EOL;
+echo $kleber->get_nome();
+echo $kleber->getIdade();
+echo $kleber->getSexo();
+
+echo "Existem ". Pessoa::getQuantidade() ." Pessoa(s) Cadastradas".PHP_EOL;
+?>
