4e22e0adef2b7a59e06df35c912b83e4a72929cb
gustavo_bolis <gustavo.bolis@ixcsoft.com.br>
2023-04-01 13:59:52 -0300

    feat(POO): Added POO EX2.

---
+++ POO/EX2.php
@@ -0,0 +1,47 @@
+<?php
+class ContaBancaria{
+    private $saldo;
+
+    public function __construct($initSaldo){
+        $this->saldo = $initSaldo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSaldo(){
+        return $this->saldo;
+    }
+
+    public function depositar($valor) {
+        $this->saldo += $valor;
+    }
+
+    public function sacar($valor) {
+        if ($valor <= $this->saldo) {
+            $this->saldo -= $valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+$conta = new ContaBancaria(1000);
+
+echo "Saldo inicial: " . $conta->getSaldo() . PHP_EOL;
+
+$conta->depositar(500);
+echo "Saldo após depósito de 500: ". $conta->getSaldo() . PHP_EOL;
+
+if ($conta->sacar(200)) {
+    echo "Saque de 200 realizado com sucesso. Saldo atual: " . $conta->getSaldo() . PHP_EOL;
+} else {
+    echo "Não foi possível realizar o saque de 200. Saldo insuficiente." . PHP_EOL;
+}
+
+if ($conta->sacar(2000)) {
+    echo "Saque de 2000 realizado com sucesso. Saldo atual: " . $conta->getSaldo() . PHP_EOL;
+} else {
+    echo "Não foi possível realizar o saque de 2000. Saldo Disponível: " . $conta->getSaldo() . PHP_EOL;
+}
